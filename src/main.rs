44518cbe2e14bd4b005857452bfdba60c17a05cc
Eric Schmale <eric.schmale@gmx.de>
2024-07-04 21:10:04 +0200

    bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use bevy::prelude::*;
+
+const SCREEN_WIDTH: f32 = 1280.0;
+const SCREEN_HEIGHT: f32 = 720.0;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "isogame".into(),
+                resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
+                ..default()
+            }),
+            ..default()
+        }),))
+        .run();
 }
