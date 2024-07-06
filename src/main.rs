87dc520417f25fb1a82704477193fb93b883f53a
Eric Schmale <eric.schmale@gmx.de>
2024-07-06 15:12:58 +0200

    diagnostics

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowTheme},
+};
 
 pub mod mapgenerator;
 
 pub const SCREEN_WIDTH: f32 = 1280.0;
 pub const SCREEN_HEIGHT: f32 = 720.0;
 
+const TICK_TIME: f32 = 1.0 / 50.0;
+
+#[derive(Component)]
+struct FpsText;
+
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "isogame".into(),
-                resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "isogame".into(),
+                    resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }),))
+            FrameTimeDiagnosticsPlugin::default(),
+        ))
+        .add_systems(FixedUpdate, (text_update_system,))
+        .insert_resource(Time::<Fixed>::from_seconds(TICK_TIME.into()))
         .add_systems(Startup, setup)
         .run();
 }
@@ -22,5 +38,38 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
+    commands.spawn((
+        // Create a TextBundle that has a Text with a list of sections.
+        TextBundle::from_sections([
+            TextSection::new(
+                "FPS: ",
+                TextStyle {
+                    font_size: 20.0,
+                    color: Color::WHITE,
+                    ..Default::default()
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font_size: 20.0,
+                ..Default::default()
+            }),
+        ]),
+        FpsText,
+    ));
+
     crate::mapgenerator::create_test_map(&mut commands, &asset_server);
+}
+
+fn text_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                // Update the value of the second section
+                text.sections[1].value = format!("{value:.2}");
+            }
+        }
+    }
 }
